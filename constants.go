338f01f2693aafac3242b5b9217cec2348294672
Bem Silva <vitorfbs.ti@gmail.com>
2024-09-05 08:56:09 -0300

    feat: move constants to different file

---
+++ constants.go
@@ -0,0 +1,9 @@
+package main
+
+func getCardValues() [13]string {
+	return [13]string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Queen", "King"}
+}
+
+func getCardSuites() [4]string {
+	return [4]string{"Spades", "Diamonds", "Hearts", "Clubs"}
+}
